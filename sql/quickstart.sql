d94023edebef319db632406ca6fde36365526cdf
grantavalon <gavalon@stanford.edu>
2018-04-22 16:41:25 -0500

    author fix

---
+++ sql/quickstart.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE rcbb_elo;
+
+USE rcbb_elo;
+
+CREATE TABLE torvik_rankings (team varchar(20), conference varchar(10), record varchar(10), 'adjoe', 'adjde', 'barthag', 'eff_fg_o', 'eff_fg_d', 
+              'turnover_o', 'turnover_d', 'off_reb_o', 'off_reb_d', 'ft_rate_o', 'ft_rate_d', 'luck', 'wab')
+
